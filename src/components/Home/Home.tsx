8fb8e841851854e7dc0c0f2e21bd629597f2ed75
Erica Hagle <ericahagle.dev@gmail.com>
2024-01-03 19:24:11 -0800

    feat: Add tracklist placeholder to main page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import Tracklist from '../Tracklist/Tracklist';
+
 const Home = () => {
   return (
     <div className="Home">
       <h1>MixTape</h1>
+      <article>
+        <Tracklist />
+      </article>
     </div>
   );
 };
---
+++ src/components/Tracklist/Tracklist.tsx
@@ -0,0 +1,11 @@
+import './Tracklist.css';
+
+import React from "react";
+
+const Tracklist = () => {
+  return (
+    <p>tracklist placeholder</p>
+  );
+};
+
+export default Tracklist;
