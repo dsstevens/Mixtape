98bbba51aaaf873a32c327688f459985a70d63c2
Erica Hagle <ericahagle.dev@gmail.com>
2024-01-03 22:17:07 -0800

    feat: Add year dropdown and some styling updates

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,14 +1,37 @@
-import React from 'react';
+import './Home.css';
+
+import React, { useState } from 'react';
 
 import Tracklist from '../Tracklist/Tracklist';
+import Dropdown from '../Dropdown/Dropdown';
 
 const Home = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
+  const handleYear = (year: number) => {
+    // add code to link to page based on the year
+    setOpen(false);
+  }
+
   return (
     <div className="Home">
-      <h1>MixTape</h1>
-      <article>
+      <h1 className='page-title'>MixTape</h1>
+
+      <Dropdown
+        open={open}
+        trigger={<button className='years-dropdown-button' onClick={handleOpen}>Albums By Year</button>}
+        menu={[1980, 1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989].map((year) => (
+          <button key={year} onClick={() => handleYear(year)}>{year}</button>
+        ))}
+      />
+
+      <div className='tracklist-container'>
         <Tracklist />
-      </article>
+      </div>
     </div>
   );
 };
--- src/components/Tracklist/Tracklist.tsx
+++ src/components/Tracklist/Tracklist.tsx
@@ -4,7 +4,19 @@ import React from "react";
 
 const Tracklist = () => {
   return (
-    <p>tracklist placeholder</p>
+    <div>
+      <h2 className='tracklist-title'>My Playlist</h2>
+      <ul className='tracklist'>
+        {/* placeholder tracks */}
+        <li className='track'>Track 1</li>
+        <li className='track'>Track 2</li>
+        <li className='track'>Track 3</li>
+        <li className='track'>Track 4</li>
+        <li className='track'>Track 5</li>
+        <li className='track'>Track 6</li>
+        <li className='track'>Track 7</li>
+      </ul>
+    </div>
   );
 };
 
