d60e5a1de03228e6f11362b0b0a830c291497aa9
Bobby Steckline <rjsturing@gmail.com>
2024-01-04 21:56:13 -0500

    Add routing to selected year page

--- src/components/AlbumsByYear/AlbumsByYear.tsx
+++ src/components/AlbumsByYear/AlbumsByYear.tsx
@@ -1,10 +1,18 @@
 import './AlbumsByYear.css';
-
 import React from 'react';
+import { useParams, Link } from 'react-router-dom';
 
 const AlbumsByYear = () => {
+  const { year } = useParams()
+
   return (
-    <p>placeholder</p>
+    <div className="albums-by-year-container">
+      <header>
+        <Link to="/" className="home-button">Home</Link>
+      </header>
+      <h2 className="year-title">Albums from the Year {year}</h2>
+            {/* grid rendering */}
+    </div>
   );
 };
 
